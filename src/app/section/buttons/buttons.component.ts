aff4b8ec735563d55202e216a7f21b65cd5727bf
i-mnam <66421797+Heesu974@users.noreply.github.com>
2022-12-31 01:14:21 +0900

    Data Binding Child to Parent

--- src/app/section/buttons/buttons.component.ts
+++ src/app/section/buttons/buttons.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+
+//1. 먼저 자식 컴포넌트 클래스 파일에 Output, EventEmitter심볼을 로드한다.
+// 2. 컴포넌트 클래스에서 프로퍼티에 @Output() 데코레이터를 지정합니다.
 
 @Component({
   selector: 'app-buttons',
@@ -6,5 +9,16 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./buttons.component.css'],
 })
 export class ButtonsComponent {
-  @Input() item = '';
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  //여기서 EventEmitter<string>은 @Output() 데코레이터의 타입입니다.
+
+  //new EventEmitter<string>은, 문자열 타입으로 이벤트를 전달하는 EventEmitter 인스턴스를 생성합니다.
+
+  //3. 자식 컴포넌트에 addNewItem() 메서드를 추가합니다.
+  addNewItem(value: string) {
+    this.newItemEvent.emit(value);
+  }
+  //click 이벤트가 바인딩된 button 엘리먼트.
+  // 이 요소의 (click) 이벤트는 자식 컴포넌트 클래스의 addNewItem() 메서드와 바인딩되어 있다. addNewItem() 에서드는 #newItem.value 값을 인자로 받습니다.
 }
--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -6,13 +6,23 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./section.component.css'],
 })
 export class SectionComponent {
-  @Input() item = '';
+  items = ['item1', 'item2', 'item3', 'item4'];
 
-  currentItem = 'Binding data From Parent component to Child component';
+  addItem(newItem: string) {
+    this.items.push(newItem);
+  }
+  //부모 컴포넌트의 addItem() 메서드는 인자로 받은 문자열을 items 배열에 저장합니다.
 }
-//그 다음으로, 부모 컴포넌트 템플릿에서 프로퍼티를 바인딩해야 합니다.
 
-//1. 부모 컴포넌트 템플릿에 자식 컴포넌트 셀렉터
-//app-time-display 를 추가한다.
+// 부모 컴포넌트로 데이터 전달
 
-//4. 부모 컴포넌트 클래스에서 currentItem 값을 할당합니다.
+//자식 컴포넌트에 @Output() 데코레이터를 사용하면 부모 컴포넌트로 데이터를 전달할 수 있다.
+
+//@Output() 데코레이터는 자식 컴포넌트의 프로퍼티 중 부모 컴포넌트로 데이터를 보내는 프로퍼티를 지정한다.
+
+//그리고 나면 자식 컴포넌트에서 이벤트를 발생시켜, 부모 컴포넌트로 보내면 된다.
+//**이벤트를 발생시키기 위해 @Output() 데코레이터는 반드시 EventEmitter 타입의 프로퍼티에 선언해야 한다. 이 커스텀 이벤트 클래스는 @angular/core 패키지에서 제공하는 클래스다. */
+
+//이제, 자식 컴포넌트 템플릿에 있는 html input 요소에서 데이터를 받아 부모 컴포넌트로 전달하는 예제를 알아봅시다.
+
+//목표 - 사용자가 input 요소에 문자열을 입력하고, 버튼을 선택해서, 이벤트를 발생시키면, EventEmitter를 사용해서 부모 컴포넌트로 이벤트를 전달한다.
