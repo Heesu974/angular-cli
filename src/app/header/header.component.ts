f6d75faaf3d6571a203c4c99d8c8a6e4bd2bc69d
i-mnam <66421797+Heesu974@users.noreply.github.com>
2022-12-30 23:18:49 +0900

    웹 스톱워치 레이아웃 컴포넌트 생성

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {}
+
+//모듈에 반드시 컴포넌트를 설정해야 합니다.
