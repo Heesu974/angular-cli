814f5382489737f82fb55173edde2f4a8b84e8a3
i-mnam <66421797+Heesu974@users.noreply.github.com>
2022-12-30 23:50:35 +0900

    동일 컴포넌트 내에서의 데이터 바인딩

--- src/app/section/buttons/buttons.component.ts
+++ src/app/section/buttons/buttons.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
-  styleUrls: ['./buttons.component.css']
+  styleUrls: ['./buttons.component.css'],
 })
 export class ButtonsComponent {
-
+  count = 0;
+  start() {
+    this.count = this.count + 1;
+  }
 }
--- src/app/section/time-display/time-display.component.ts
+++ src/app/section/time-display/time-display.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-time-display',
   templateUrl: './time-display.component.html',
-  styleUrls: ['./time-display.component.css']
+  styleUrls: ['./time-display.component.css'],
 })
 export class TimeDisplayComponent {
+  test = 1;
 
+  constructor() {
+    setInterval(() => {
+      this.test = this.test + 1;
+    }, 1000);
+  }
 }
